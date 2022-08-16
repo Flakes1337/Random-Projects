2ad2cd090a70dbcdf660d34be671a00a38ab415c
Flakes1337 <111360415+Flakes1337@users.noreply.github.com>
2022-08-16 08:45:59 -0400

    Add files via upload

---
+++ Short-Story.py
@@ -0,0 +1,13 @@
+name = input("Enter your name: ")
+print("Silly name", name + "!")
+
+age = input("How old are you?: ")
+print("yikes", age + "!")
+
+gender = input("what is your gender?: ")
+print("gross", gender + "!")
+
+print ("There once was a " + gender + " named " + name + ", ")
+print ("" + gender + " was " + age + " years old.")
+print ("" + gender + " really liked the name " + name + ",")
+print ("But didnt like being " + age + ".")
