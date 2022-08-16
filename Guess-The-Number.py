334c2e1258c1e51d7e854a0d24c35e54aace188e
Brandon <111360415+Flakes1337@users.noreply.github.com>
2022-08-16 14:34:21 -0400

    Add files via upload

---
+++ Guess-The-Number.py
@@ -0,0 +1,27 @@
+#select random number
+import random
+num = random.randint(1,10000)
+print(num)
+
+#accept guess, decide if win or loss
+run = True
+while run:
+    guess = int(input("Guess a number between 1-10000: "))
+    print("|")
+    if guess == int(num):
+        print("Good job, you win!")
+        print("|")
+        run = False
+    else:
+        if guess >= int(num):
+            print("Too high!")
+            print("|")
+        if guess <= int(num):
+            print("Too low!")
+            print("|")
+        continue
+    playagain = int(input("Play again? 1=yes, 2=no: "))
+    if playagain == int(2):
+        run = False
+    else:
+        run = True
