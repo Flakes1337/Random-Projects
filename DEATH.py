99dea670a58bc6cdc3883ecfe4251c280f3f1038
Brandon <111360415+Flakes1337@users.noreply.github.com>
2022-08-17 15:44:59 -0400

    Add files via upload

---
+++ DEATH.py
@@ -0,0 +1,81 @@
+import sys
+import time
+
+#Functions for typing effect
+def typingPrint(text):
+  for character in text:
+    sys.stdout.write(character)
+    sys.stdout.flush()
+    time.sleep(0.05)
+  
+def typingInput(text):
+  for character in text:
+    sys.stdout.write(character)
+    sys.stdout.flush()
+    time.sleep(0.05)
+  value = input()  
+  return value
+
+#Welcome screen
+print("        .'''''.        ..||..'''''''...")
+print("       / ##### \       : ||            ''.")
+print("      | ## # ## |      :.||...''''''....  '.")
+print("      | #  #  # |        ||             ''''")
+print("      | ####### |     .  ||")
+print("       \ ##### /     /| < _>")
+print("        \ ### /     / |/< _>")
+print("      ..''   ''... /  | < _>")
+print("    .'            /   | /||             Hello fool.")
+print("    '                 |/ ||        I am the grim reaper.")
+print("    |   |     '..     |  ||")
+print("    |   |     |  '...''  ||     Today I will be determining")
+print("    |  |       |         ||        THE DAY YOU WILL DIE.")
+print("     \ |       |         ||")
+print("     |\|       |         || Just answer a few simple questions.")
+print("     \|         |        ||             if you dare.")
+print("      |         |        ||")
+print("      |         |        ||")
+print("     |           |       ||")
+print("   __|           |__     ||")
+print("  /   '.........'   \    ||")
+print("   ''''''     ''''''     ##")
+print("---------------------------------------------------------------")
+
+#questions
+name = typingInput("What is thy name?: ")
+birthyear = typingInput("Now tell me your birth year: ")
+birthmonth = typingInput("Now your birth month: ")
+birthday = typingInput("Tell me the day you were born: ")
+print("------------------------------------")
+
+#Repeating results
+print("Ah, so your name is " + name + " and you were born on the " + birthday + " of " + birthmonth + " in the year " + birthyear + "...")
+print("\nHow interesting...")
+print("------------------------------------")
+
+#fake loading bar
+loading= "CALCULATING TIME OF DEATH... Please hold...\n"
+bar = "[=============================]"
+print(loading)
+for c in bar:
+    time.sleep(0.5)
+    sys.stdout.write(c)
+    sys.stdout.flush()
+    
+#jokey joke
+typingPrint("\nOh? \nThat cant be right.... \nSucks to be you lol.")
+
+#Results
+print("   _                   _")
+print(" _( )                 ( )_")
+print("(_, |      __ __      | ,_)")
+print("   \'\    /  ^  \    /'/")
+print("    '\'\,/\      \,/'/'")
+print("      '\| []   [] |/'")
+print("        (_  /^\  _)")
+print("          \  ~  /")
+print("          /HHHHH\\")
+print("        /'/{^^^}\'\\")
+print("    _,/'/'  ^^^  '\'\,_")
+print("   (_, |           | ,_)")
+print("     (_)           (_)")
