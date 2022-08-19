0ed761b4d84bcd7f14aff8666711f66c7d77c49a
Brandon <111360415+Flakes1337@users.noreply.github.com>
2022-08-19 16:22:40 -0400

    Update DEATH.py

--- DEATH.py
+++ DEATH.py
@@ -3,23 +3,22 @@ import sys
 import time
 import requests
 from bs4 import BeautifulSoup
-
+import random
 
 #functions for cool typing effect
 def typingPrint(text):
   for character in text:
     sys.stdout.write(character)
     sys.stdout.flush()
-    time.sleep(0.04)
+    time.sleep(0.02)
   
 def typingInput(text):
   for character in text:
     sys.stdout.write(character)
     sys.stdout.flush()
-    time.sleep(0.04)
+    time.sleep(0.02)
   value = input()  
   return value
-
 
 #Welcome screen
 print("        .'''''.        ..||..'''''''...")
@@ -46,64 +45,54 @@ print("  /   '.........'   \    ||")
 print("   ''''''     ''''''     ##")
 print("---------------------------------------------------------------")
 
-
-#ask user for birthday and translate for program
-
+#ask user for name, birthday and gender
 name = typingInput("Tell me your name: ")
-
 year = typingInput("What is your birth year?: ")
-
-user_month = typingInput("What is your birth month?: ")
-if user_month == ("January"):
-    month = ("01")
-elif user_month == ("january"):
-    month = ("01")
-elif user_month == ("February"):
-    month = ("02")
-elif user_month == ("february"):
-    month = ("02")
-elif user_month == ("March"):
-    month = ("03")
-elif user_month == ("march"):
-    month = ("03")
-elif user_month == ("April"):
-    month = ("04")
-elif user_month == ("april"):
-    month = ("04")
-elif user_month == ("May"):
-    month = ("05")
-elif user_month == ("may"):
-    month = ("05")
-elif user_month == ("June"):
-    month = ("06")
-elif user_month == ("june"):
-    month = ("06")
-elif user_month == ("July"):
-    month = ("07")
-elif user_month == ("july"):
-    month = ("07")
-elif user_month == ("August"):
-    month = ("08")
-elif user_month == ("august"):
-    month = ("08")
-elif user_month == ("September"):
-    month = ("09")
-elif user_month == ("september"):
-    month = ("09")
-elif user_month == ("October"):
-    month = ("10")
-elif user_month == ("october"):
-    month = ("10")
-elif user_month == ("November"):
-    month = ("11")
-elif user_month == ("november"):
-    month = ("11")    
-elif user_month == ("December"):
-    month = ("12")
-elif user_month == ("december"):
-    month = ("12")
-
+monthinput = typingInput("What month were you born in?: ")
 user_day = int(typingInput("What day were you born on?: "))
+genderinput = typingInput("Are you male or female?: ")
+
+#sort what user typed as birth month
+JAN = ["Jan", "jan", "january", "January"]
+FEB = ["February", "february", "Feb", "feb"]
+MAR = ["March", "march", "Mar", "mar"]
+APR = ["April", "april", "Apr", "apr"]
+MAY = ["May", "may"]
+JUN = ["June", "june", "Jun", "jun"]
+JUL = ["July", "july", "Jul", "jul"]
+AUG = ["August", "august", "Aug", "aug"]
+SEP = ["September", "september", "Sept", "sept", "Sep", "sep"]
+OCT = ["October", "october", "Oct", "oct"]
+NOV = ["November", "november", "Nov", "nov"]
+DEC = ["December", "december", "Dec", "dec"]
+
+#turn birth month into a digit
+if any(keyword in monthinput for keyword in JAN):
+    month = ("01")
+elif any(keyword in monthinput for keyword in FEB):
+    month = ("02")
+elif any(keyword in monthinput for keyword in MAR):
+    month = ("03")
+elif any(keyword in monthinput for keyword in APR):
+    month = ("04")
+elif any(keyword in monthinput for keyword in MAY):
+    month = ("05")
+elif any(keyword in monthinput for keyword in JUN):
+    month = ("06")
+elif any(keyword in monthinput for keyword in JUL):
+    month = ("07")
+elif any(keyword in monthinput for keyword in AUG):
+    month = ("08")
+elif any(keyword in monthinput for keyword in SEP):
+    month = ("09")
+elif any(keyword in monthinput for keyword in OCT):
+    month = ("10")
+elif any(keyword in monthinput for keyword in NOV):
+    month = ("11")
+elif any(keyword in monthinput for keyword in DEC):
+    month = ("12")
+
+#turn what user typed as day, turn it into digit
 if user_day <= 10:
     if user_day == 1:
         day = ("01")
@@ -125,38 +114,33 @@ if user_day <= 10:
         day = ("09")
 if user_day >= 10:
     day = (str(user_day))
-
-gender = typingInput("Are you male or female?: ")
-
+    
+#take genderinput and turn it into number
+MAN = "M", "m", "B", "b", "D", "d", "G", "g"
+WOMAN = "W", "w", "F", "f", "G", "g", "Gurl"
+if any(keyword in genderinput for keyword in MAN):
+    gender = 76
+if any(keyword in genderinput for keyword in WOMAN):
+    gender = 81
 
 #webscrape data
-
 url = "https://happyhappybirthday.net/en/" + year + "/" + month + "/" + day + ""
 page = requests.get(url)
 soup = BeautifulSoup(page.content, "html.parser")
 
-
 #parse for fun facts
-
 week_day = soup.find("div", class_="col-md-4 col-sm-4 text-center wochentag")
 weekday = week_day.find("p")
-
 yearsold = soup.find("div", class_="years-old")
-
 monthsold = soup.find("h3", class_="bg-green")
-
 weeksold = soup.find("h3", class_="bg-lblue")
-
 daysold = soup.find("h3", class_="bg-red")
-
 zodiac_sign = soup.find("div", class_="col-md-4 col-sm-4 text-center sternzeichen")
 zodiacsign = zodiac_sign.find("h3")
 
 #shared_birthday1 = soup.find("div", class_="col-sm-6")
 
-
 #print fun facts
-
 print("-----------------------------")
 typingPrint(weekday.text + "\n")
 typingPrint("You have been alive for:\n")
@@ -169,26 +153,22 @@ print("-----------------------------")
 #print("you share birthdays with:")
 #print(shared_birthday1.text)
 
-
 #fake loading bar
-
 loading= "CALCULATING TIME OF DEATH... Please hold...\n"
-bar = "[===========================================]"
+bar = "[======================================]"
 print(loading)
 for c in bar:
-    time.sleep(0.5)
+    time.sleep(0.3)
     sys.stdout.write(c)
     sys.stdout.flush()
 
-
-#death results
-
+#estimate time of death and show results
 print("\n   _                   _")
 print(" _( )                 ( )_")
 print("(_, |      __ __      | ,_)")
-print("   \'\    /  ^   \    /'/")
-print("    '\'\,/\       \,/'/'")
-print("      '\| []   [] |/'")
+print("   \\'\    /  ^   \    /'/")
+print("    '\\'\,/\       \,/'/'")
+print("      '\\| []   [] |/'")
 print("        (_  /^\  _)")
 print("          \  ~  /")
 print("          /XXXXX\\")
@@ -196,7 +176,21 @@ print("        /'/{^^^}\'\\")
 print("    _,/'/'  ^^^  '\'\,_")
 print("   (_, |           | ,_)")
 print("     (_)           (_)")
+print("------------------------------\n")
 
-if gender == ("Male") or ("male"):
-    76 - int(yearsold.text.strip()) = int(maledeath)
-    print(maledeath)
+#pick death year
+Deathlist = int(year) + int(gender)
+Deathyear = (str(Deathlist))
+
+#pick random month
+Monthlist = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+Deathmonth = random.choice(Monthlist)
+
+#pick random day
+Daylist = ["1st", "2nd", "3rd", "4th", "5th", "6th", "7th", "8th", "9th", "10th", "11th", "12th", "13th", "14th", "15th", "16th", "17th", "18th", "19th", "20th", "21st", "22nd", "23rd", "24th", "25th", "26th", "27th", "28th", "29th", "30th"]
+Deathday = random.choice(Daylist)
+
+print(name + ", it seems that your fate is sealed.")
+print("Your life will end on the " + Deathday + " of " + Deathmonth + " in the year " + Deathyear + ".")
+print("-------------------------------------------")
+print("Enjoy the time you have left, have a nice day!")
